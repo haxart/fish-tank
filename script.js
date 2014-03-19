e6b67ce414a1b17c9f87c83fed4bd1f7960d66cf
Christian Flores <christianedit@gmail.com>
2014-03-19 17:39:20 -0400

    dirty fish cloning MVP

---
+++ script.js
@@ -0,0 +1,7 @@
+// Quick and dirty fish cloning
+document.getElementById('fish').onclick = function() {
+	var fish = document.getElementById('fish');
+    var clone = fish.cloneNode(true); // "deep" clone
+    // get parent node, append fish to it.
+    fish.parentNode.appendChild(clone);
+}
